de6130e671d2b91efa356a779686ec62b6f067fa
chasedeger <cdeger88@gmail.com>
2017-09-08 16:41:32 -0700

    first commit-html-done

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+var playCircles = $('.cell');
+var reset = $('#reset');
+
+var row1 = $('div.row1');
+var row2 = $('div.row2');
+var row3 = $('div.row3');
+var row4 = $('div.row4');
+var row5 = $('div.row5');
+var row6 = $('div.row6');
